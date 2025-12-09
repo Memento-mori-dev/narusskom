0b22bbfb82b7a3115623cc0d8e380c84a70b66f5
Kirill <job.mori.1967@gmail.com>
2025-12-09 18:41:31 +0300

    everyone адаптив

--- frontend-vite/layout/src/main.js
+++ frontend-vite/layout/src/main.js
@@ -1,3 +1,30 @@
 import { useDynamicAdapt } from './script/dynamicAdapt';
 
 useDynamicAdapt();
+
+const swiper = new Swiper('.everyone', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+    breakpoints: {
+        0:   { 
+            slidesPerView: 1.1,
+            spaceBetween: 12,
+        },
+        560:   { 
+            slidesPerView: 1.8,
+            spaceBetween: 12,
+        },
+        760:   { 
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1100:   { 
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+    }
+});
