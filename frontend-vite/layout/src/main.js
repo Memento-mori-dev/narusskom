5ad44b511662dd0ab0051f4500abb3bbc6d78e22
Kirill <job.mori.1967@gmail.com>
2025-12-10 17:21:06 +0300

    history pc

--- frontend-vite/layout/src/main.js
+++ frontend-vite/layout/src/main.js
@@ -2,7 +2,7 @@ import { useDynamicAdapt } from './script/dynamicAdapt';
 
 useDynamicAdapt();
 
-const swiper = new Swiper('.everyone', {
+const swiperEveryone = new Swiper('.everyone', {
     slidesPerView: 3,
     spaceBetween: 20,
     // And if we need scrollbar
@@ -27,4 +27,35 @@ const swiper = new Swiper('.everyone', {
             spaceBetween: 20,
         },
     }
+});
+
+const swiperHistory = new Swiper('.history', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    // And if we need scrollbar
+    breakpoints: {
+        0:   { 
+            slidesPerView: 1.1,
+            spaceBetween: 12,
+        },
+        560:   { 
+            slidesPerView: 1.8,
+            spaceBetween: 12,
+        },
+        760:   { 
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1023:   { 
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+    },
+    navigation: {
+        nextEl: '.history-controller .next',
+        prevEl: '.history-controller .prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
 });
