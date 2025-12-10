ffb8eb4fc415b2b3b00c370153c456f8fda5dba0
Kirill <job.mori.1967@gmail.com>
2025-12-10 21:19:36 +0300

    fix

---
+++ frontend-vite/layout/src/main.js
@@ -0,0 +1,90 @@
+import { useDynamicAdapt } from './script/dynamicAdapt.js';
+import { Menu } from './script/Menu.js';
+import { Tabs } from './script/Tabs.js';
+import { Answers } from './script/Answers.js';
+import { History } from './script/History.js';
+
+useDynamicAdapt();
+
+// Подключение классов для блоков
+
+if (document.querySelector('.header')) {
+    new Menu('.header', '[data-js-open-menu]', '[data-js-menu-wrapper]', '[data-js-menu-content]');
+}
+
+if (document.querySelector('.header__lang')) {
+    new Menu('[data-js-lang]', '[data-js-lang-btn]', '[data-js-lang-wrapper]', '[data-js-lang-content]');
+}
+
+if (document.querySelector('.tabs')) {
+    new Tabs('[data-js-tab]', '[data-js-tab-btn]', '[data-js-tab-state]', '[data-js-tab-item]');
+
+    new Menu('[data-js-tabs-list]', '[data-js-tabs-list-open]', '[data-js-tabs-list-wrapper]', '[data-js-tabs-list-content]');
+}
+
+if (document.querySelector('.answers')) {
+    new Answers('[data-js-answers]', '[data-js-answers-item]', '[data-js-answers-btn]', '[data-js-answers-wrapper]', '[data-js-answers-content]')
+}
+
+if (document.querySelector('.history')) {
+    new History('.history', '.history__item--short', '.history__item--short .btn--open-review', '.history__item--short .history__item-description', '.history__item--short .history__item-description-text');
+}
+
+// swiper
+const swiperEveryone = new Swiper('.everyone', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+    breakpoints: {
+        0:   { 
+            slidesPerView: 1.1,
+            spaceBetween: 12,
+        },
+        560:   { 
+            slidesPerView: 1.8,
+            spaceBetween: 12,
+        },
+        760:   { 
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1023:   { 
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+    }
+});
+
+const swiperHistory = new Swiper('.history', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    // And if we need scrollbar
+    breakpoints: {
+        0:   { 
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        560:   { 
+            slidesPerView: 1.8,
+            spaceBetween: 20,
+        },
+        760:   { 
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1023:   { 
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+    },
+    navigation: {
+        nextEl: '.history-controller .next',
+        prevEl: '.history-controller .prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
---
+++ frontend-vite/layout/src/script/History.js
@@ -0,0 +1,100 @@
+export class History{
+    stateClasses = {
+        isActive: 'is-active',
+    }
+    
+    state = {
+        move: false,
+    }
+
+    constructor(main, item, btnItem, wrapperItem, contentItem){
+        this.main = document.querySelector(main);
+        this.arrItem = this.main.querySelectorAll(item);
+        this.arrBtnItem = this.main.querySelectorAll(btnItem);
+        this.arrWrapperItem = this.main.querySelectorAll(wrapperItem);
+        this.arrContentItem = this.main.querySelectorAll(contentItem);
+
+        this.init();
+    }
+
+    init(){
+        this.trigger();
+    }
+
+    trigger(){
+        this.arrBtnItem.forEach((btn, index) => {
+            btn.onclick = () => {
+                if (this.state.move) return;
+
+                this.stateSwitch();
+
+                this.toggle(index);
+
+                this.stateSwitch();
+            }
+        });
+    }
+
+    toggle(index){
+        this.arrItem.forEach((item, indexThis) => {
+            if (index == indexThis) {
+                if (!item.classList.contains(this.stateClasses.isActive)) {
+                    this.open(indexThis, item);
+                }else{
+                    this.close(indexThis, item);
+                }
+                
+            }else{
+                if (item.classList.contains(this.stateClasses.isActive)) {
+                    this.close(indexThis, item);
+                }
+            }
+        });
+    }
+
+    open(index, item){
+        const wrapper = this.arrWrapperItem[index];
+        const content = this.arrContentItem[index];
+
+        item.classList.add(this.stateClasses.isActive);
+
+        const newHeight = content.offsetHeight + 'px';
+
+        wrapper.style.height = newHeight;
+
+        this.setTimeout(()=> {
+            wrapper.style.height = 'auto';
+        })
+    }
+
+    close(index, item){
+        const wrapper = this.arrWrapperItem[index];
+        const content = this.arrContentItem[index];
+
+        item.classList.remove(this.stateClasses.isActive);
+
+        const newHeight = content.offsetHeight + 'px';
+
+        wrapper.style.height = newHeight;
+
+        this.setTimeout(() =>{
+            wrapper.style.height = 124 + 'px';
+        })
+    }
+
+    stateSwitch(){
+        if (!this.state.move){
+            this.state.move = true;
+        }else{
+            this.setTimeout(() => {
+                this.state.move = false;
+            });
+        }
+    }
+
+    setTimeout(fn){
+        setTimeout(() => {
+            fn();
+        }, 200);
+    }
+}
