0fa0449c91cebfd5f54d8ca8caa18df008f6a06e
Kirill <job.mori.1967@gmail.com>
2025-12-23 19:37:33 +0300

    Пословицы и поговорки адаптив

--- frontend-vite/layout/src/script/sliders.js
+++ frontend-vite/layout/src/script/sliders.js
@@ -258,4 +258,22 @@ export function sliders(width) {
             });
         });
     }
+
+    if (document.querySelector('.long-proverbs')) {
+        const ARR_LONG_SLIDER_PROVERBS = document.querySelectorAll('.long-proverbs');
+
+        ARR_LONG_SLIDER_PROVERBS.forEach((slider, index) => {
+            new Swiper(slider, {
+                slidesPerView: 1,
+                spaceBetween: 100,
+                pagination: {
+                    el: '.swiper-pagination',
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+            });
+        });
+    }
 }
